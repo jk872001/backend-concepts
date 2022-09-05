0c726078b5317c3232417b8ce8b67996b766e6b3
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-09-06 01:13:13 +0530

    password encryption done

--- auth-system/app.js
+++ auth-system/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("./config/database").connect();
 const express=require("express");
 const app=express();
+var bcrypt = require('bcryptjs');
 const User=require("./model/user")
 app.use(express.json());
 
@@ -20,10 +21,20 @@ app.get("/register",async(req,res)=>
     }
 
     const existingUser= await User.findOne({email})
+
     if(existingUser)
     {
         res.status(400).send("User Already Exists");
     }
+
+    const safePassword= await bcrypt.hash(password,10);
+
+    const user = await User.create({
+        firstName,
+        lastName,
+        email:email.toLowerCase(),
+        password:safePassword
+    })
 })
 
 module.exports=app;
