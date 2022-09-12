bbfd2b5be76bc6d1b6d1778ac80c918ba7f6f4d8
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-09-13 00:42:35 +0530

    myget request added

--- forms-image/app.js
+++ forms-image/app.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 
 const express=require("express")
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-
-
+app.get("/myget",(req,res)=>{
+    res.send(req.body)
+})
 
 
 
