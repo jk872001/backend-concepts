9071a0ed8ae8a2e92ff146f11c596c3d127a910b
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-09-15 12:46:01 +0530

    get and post form created

--- forms-image/app.js
+++ forms-image/app.js
@@ -1,15 +1,23 @@
 require("dotenv").config();
-
-const express=require("express")
-const app=express();
+const express = require("express");
+let ejs = require("ejs");
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/myget",(req,res)=>{
-    res.send(req.body)
-})
+// using ejs middleware
+app.set("view engine", "ejs");
 
+app.get("/myget", (req, res) => {
+  res.send(req.body);
+});
 
+app.get("/mygetform", (req, res) => {
+  res.render("getForm");
+});
 
+app.get("/mypostform", (req, res) => {
+  res.render("postForm");
+});
 
-module.exports=app;
+module.exports = app;
